cf1f67ea18c874f0fe39682c993bf093421d4bd7
geotorch <rukmangadan.d@gmail.com>
2024-11-03 20:30:27 +0530

    p

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 import os
+import sys
+import time
 
 import keyboard
 from pydub import AudioSegment
 from pydub.playback import play
 
 data_dir = "D:/PROJECTS"
-audio_file = "flow.mp3"
-code_file = "main.py"
+audio_file = os.path.join(data_dir, "flow.mp3")
+coder_file = os.path.join(data_dir, "main.py")
 
-# Unmapped VS Code Shortcuts
-# Ctrl + Shift + Q, I, A, J, L
+"""
+Unmapped VS Code Shortcuts
+Ctrl + Shift + Q, I, A, J, L
 
-# audio = AudioSegment.from_mp3("D:/PROJECTS/flow.mp3")
-# play(audio)
+Start Recording - Ctrl + Shift + J
+Close Recording - Ctrl + Shift + L
+"""
+
+toExit = False
+
+def record_start():
+    print("start recording")
+    # audio = AudioSegment.from_mp3("D:/PROJECTS/flow.mp3")
+    # play(audio)
+
+def record_close():
+    global toExit
+    print("close recording")
+    keyboard.unhook_all_hotkeys()
+    toExit = True
+    
+
+keyboard.add_hotkey('Ctrl+Shift+J', record_start)
+keyboard.add_hotkey('Ctrl+Shift+L', record_close)
+
+while not toExit:
+    print("listening")
+    time.sleep(1)
+
