b4dac0308beef7dfcf2f8dbaaa9ebc9e19b53959
geotorch <rukmangadan.d@gmail.com>
2024-11-03 19:38:06 +0530

    p

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ data_dir = "D:/PROJECTS"
 audio_file = "flow.mp3"
 code_file = "main.py"
 
+# Unmapped VS Code Shortcuts
+# Ctrl + Shift + Q, I, A, J, L
 
 # audio = AudioSegment.from_mp3("D:/PROJECTS/flow.mp3")
 # play(audio)
