6c8c8c030f9336d030ca8e21424c402562e357c0
geotorch <rukmangadan.d@gmail.com>
2024-11-03 23:42:35 +0530

    p

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
 import time
-import multiprocessing
-
+import threading
 import keyboard
 from pydub import AudioSegment
 from pydub.playback import play
 
 data_dir = "D:/PROJECTS"
 audio_file = os.path.join(data_dir, "flow.mp3")
-coder_file = os.path.join(data_dir, "main.py")
 
 """
 Unmapped VS Code Shortcuts
@@ -22,30 +20,30 @@ toExit = False
 recordStarted = False
 
 def play_audio():
-    audio = AudioSegment.from_mp3(audio_file)
-    play(audio)
+    try:
+        audio = AudioSegment.from_mp3(audio_file)
+        play(audio)
+    except Exception as e:
+        print(f"Error playing audio: {e}")
 
 def record_start():
     global recordStarted
     if not recordStarted:
-        time.sleep(1)
-        audio_process = multiprocessing.Process(target=play_audio)
-        audio_process.start()
-
-    recordStarted = True
-    print("started recording")
+        print("Starting audio playback")
+        audio_thread = threading.Thread(target=play_audio)
+        audio_thread.start()
+        recordStarted = True
+        print("Started recording")
 
 def record_close():
     global toExit
-    print("close recording")
+    print("Closing recording")
     keyboard.unhook_all_hotkeys()
     toExit = True
-    
 
 keyboard.add_hotkey('Ctrl+Shift+J', record_start)
 keyboard.add_hotkey('Ctrl+Shift+L', record_close)
 
 while not toExit:
-    print("listening")
+    print("Listening for hotkeys...")
     time.sleep(1)
-
